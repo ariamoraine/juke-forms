d1074fc6d196db5c70b293fed0d4773d4ad89faa
Aria Moraine <ariamoraine@gmail.com>
2017-05-17 15:48:04 -0400

    adding in dummy playlists into sidebar

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -20,6 +20,14 @@ const Sidebar = (props) => {
       <section>
         <h4 className="text-muted">PLAYLISTS</h4>
         <h4>
+          <ul className="list-unstyled">
+            <li className="playlist-item menu-item">
+              <Link to="FILL_ME_IN">some playlist</Link>
+            </li>
+            <li className="playlist-item menu-item">
+              <Link to="WHERE_TO_GO">another playlist</Link>
+            </li>
+          </ul>
           <Link className="btn btn-primary btn-block" to='/newplaylist'>
             <span className="glyphicon glyphicon-plus">PLAYLIST</span>
           </Link>
